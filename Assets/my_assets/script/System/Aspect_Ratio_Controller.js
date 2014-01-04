83be2735f263cad21242cf55b147ccd88e58184a
nutti <nutti.metro@gmail.com>
2014-01-04 22:15:44 +0900

    Development title.

---
+++ Assets/my_assets/script/System/Aspect_Ratio_Controller.js
@@ -0,0 +1,51 @@
+﻿#pragma strict
+
+var height : float;
+var width : float;
+var ratio : float;
+var rect : Rect;
+var enable : boolean;
+
+height = 960.0f;
+width = 640.0f;
+
+function Update()
+{
+	var cam : Camera;
+	var targetAspect : float;
+	var curAspect : float;
+
+	if( !enable ){
+		return;
+	}
+
+	cam = gameObject.GetComponent( Camera );
+	
+	if( !cam ){
+		return;
+	}
+	
+	targetAspect = width / height;
+	curAspect = Screen.width * 1.0f / Screen.height;
+	ratio = curAspect / targetAspect;
+	
+	if( 1.0f > ratio ){
+		cam.rect.x = 0.0f;
+		cam.rect.width = 1.0f;
+		cam.rect.y = ( 1.0f - ratio ) / 2.0f;
+		cam.rect.height = ratio;
+	}
+	else{
+		ratio = 1.0f / ratio;
+		cam.rect.x = ( 1.0f - ratio ) / 2.0f;
+		cam.rect.width = ratio;
+		cam.rect.y = 0.0f;
+		cam.rect.height = 1.0f;
+	}
+	
+	rect = cam.rect;
+	
+	//Destroy( this );
+}
+
+@script AddComponentMenu( "RashFlight/AspectRatioController" )
---
+++ Assets/my_assets/script/System/Cross_Platform_GUI_Controller.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+
+var x : float = 0.0f;
+var y : float = 0.0f;
+var width : float = 1.0f;
+var height : float = 1.0f;
+
+var screen : Vector3;
+
+var cam : GameObject;
+
+var enable : boolean;
+
+function Update()
+{
+	var texture : GUITexture;
+	
+	texture = GetComponent( GUITexture );
+	
+	if( !texture ){
+		return;
+	}
+
+	if( !cam || !enable ){
+		screen.Set( Screen.width, Screen.height, 1.0f );
+		texture.pixelInset.x = x * Screen.width;
+		texture.pixelInset.y = y * Screen.height;
+		texture.pixelInset.width = width * Screen.width;
+		texture.pixelInset.height = height * Screen.height;
+	}
+	else{
+		var rect : Rect;
+		var h : float;
+		var w : float;
+		rect = cam.GetComponent( Aspect_Ratio_Controller ).rect;
+		h = cam.GetComponent( Aspect_Ratio_Controller ).height;
+		w = cam.GetComponent( Aspect_Ratio_Controller ).width;
+		texture.pixelInset.x = ( x * rect.width ) * Screen.width;
+		texture.pixelInset.y = ( y * rect.height ) * Screen.height;
+		texture.pixelInset.width = width * rect.width * Screen.width;
+		texture.pixelInset.height = height * rect.height * Screen.height;
+	}
+}
+
+
+@script AddComponentMenu( "RashFlight/System/CrossPlatformGUIController" )
